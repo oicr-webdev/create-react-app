7e6cf430bdb18cade33d71f438197d730c30adf1
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-10-23 16:11:38 -0400

    Add ProviderPlugin for jQuery scope problem

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -727,6 +727,10 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
